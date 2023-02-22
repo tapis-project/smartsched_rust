3679902cfeb7832ecaf5d4b153fc3fdea9866fa1
Richard Cardone <rcardone@tacc.utexas.edu>
2023-02-22 16:10:53 -0600

    record in, record out version (db driver problem)

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ use shellexpand;
 // The file that contains the complete mysql database url, including password.
 const DB_URL_FILE: &str = "~/smartsched-db.url";
 const DEFAULT_OUTPUT_TABLE: &str = "jobq_history";
+const DEFAULT_COMMIT_BATCH_SIZE: i32 = 10000;
 
 // ***************************************************************************
 //                                  Structs
@@ -96,14 +97,18 @@ fn main() {
                        ORDER BY submit ASC";
 
     // Connect to the database and panic if we can't.
-    let pool = Pool::new(url.as_str()).unwrap();
-    let mut conn = pool.get_conn().unwrap();
+    let pool = Pool::new(url.as_str()).expect("Failed to create pool.");
+    let mut conn1 = pool.get_conn().expect("Failed to create conn1.");
+    let mut conn2 = pool.get_conn().expect("Failed to create conn2.");
 
-    // Create the output table.
-    create_output_table(&mut conn, DEFAULT_OUTPUT_TABLE);
+    // Create the output table using the first connection.
+    // Prepare the insert statement on the second connection.
+    create_output_table(&mut conn1, DEFAULT_OUTPUT_TABLE);
+    let insert_stmt = get_insert_stmt(&mut conn2, DEFAULT_OUTPUT_TABLE);
 
     // Create the queue with a capacity not likely to be exceeded.
     let mut backlog_queue: Vec<DBsource> = Vec::with_capacity(256);
+    let mut output_records: Vec<DBtarget> =  Vec::with_capacity(4000000);
 
     // Set up the database download.
     let mut num_rows = 0;
@@ -111,15 +116,16 @@ fn main() {
     let before = Instant::now();
 
     // Iterate through the records one by one.
-    conn.query_iter(select).unwrap().for_each(|row| {
+    println!("\nIssuing query: {}", select);
+    conn1.query_iter(select).expect("query_iter() failure").for_each(|row| {
         match row {
             Ok(a) => {
                 // Decode the database row.
                 let r = from_row::<(String, String, String, i32, i32)>(a);
                 let cur_job = DBsource::new(
                     r.0,
-                    NaiveDateTime::parse_from_str(r.1.as_str(), "%Y-%m-%d %H:%M:%S").unwrap(),
-                    NaiveDateTime::parse_from_str(r.2.as_str(), "%Y-%m-%d %H:%M:%S").unwrap(),
+                    NaiveDateTime::parse_from_str(r.1.as_str(), "%Y-%m-%d %H:%M:%S").expect("Submit decode failure."),
+                    NaiveDateTime::parse_from_str(r.2.as_str(), "%Y-%m-%d %H:%M:%S").expect("Start decode failure."),
                     r.3,
                     r.4,
                 );
@@ -144,21 +150,40 @@ fn main() {
 
                 // Remove the backlog jobs marked for removal.
                 let mut iter = keep.iter();
-                backlog_queue.retain(|_| *iter.next().unwrap());
+                backlog_queue.retain(|_| *iter.next().expect("backlog.queue.retain failure"));
 
                 // Push the current ob onto the backlog queue.
                 backlog_queue.push(cur_job.clone());
 
                 // Create the current job's output record.
                 let cur_job_output = DBtarget::new(cur_job, cur_queue_minutes, cur_backlog_len);           
+                output_records.push(cur_job_output);
+                //insert_output(&mut conn2, &insert_stmt, cur_job_output);
                 
-                println!("{}", cur_job_output);
+                // Print a message every time we commit a group of output records.
                 num_rows += 1;
+                if (num_rows % DEFAULT_COMMIT_BATCH_SIZE) == 0 {
+                    println!("Database rows read = {}.", num_rows);
+                    //conn2.query_drop("COMMIT").expect("Batch commit failure.");
+                }
             },
-            Err(_) => {num_rows += 1; num_row_err += 1;},
+            Err(e) => {
+                num_rows += 1; num_row_err += 1;
+                println!("Error reading row {}: {}", num_rows, e);
+            },
         };
     });
-    println!("Elapsed time: {:.2?}", before.elapsed());
+
+    // Commit any residual rows.
+    println!("Database rows read = {}.", num_rows);
+    conn2.query_drop("COMMIT").expect("Final commit on conn2 failure."); 
+    conn1.query_drop("COMMIT").expect("Final commit on conn1 failure.");
+
+    // Create indexes after all rows loaded.
+    create_indexes(&mut conn2, DEFAULT_OUTPUT_TABLE);
+
+    // Print results.
+    println!("\nElapsed time: {:.2?}", before.elapsed());
     println!("Total rows read  = {}", num_rows);
     println!("Total row errors = {}", num_row_err);
 }
@@ -167,7 +192,7 @@ fn main() {
 // load_db_url:
 // ---------------------------------------------------------------------------
 fn load_db_url() -> String {
-    fs::read_to_string(get_absolute_path(DB_URL_FILE)).unwrap()
+    fs::read_to_string(get_absolute_path(DB_URL_FILE)).expect("Failed to load mysql url from file.")
 }
 
 // ---------------------------------------------------------------------------
@@ -176,7 +201,7 @@ fn load_db_url() -> String {
 fn create_output_table(conn: &mut PooledConn, table_name: &str) {
 
     // Drop the output table if it already exists.
-    conn.query_drop("DROP TABLE IF EXISTS ".to_owned() + table_name).unwrap();
+    conn.query_drop("DROP TABLE IF EXISTS ".to_owned() + table_name).expect("Drop table failure.");
 
     // Create a new table.
     let create_table = "CREATE TABLE :table_name \
@@ -188,15 +213,59 @@ fn create_output_table(conn: &mut PooledConn, table_name: &str) {
                              backlog_minutes int unsigned NOT NULL, \
                              backlog_num_jobs int unsigned NOT NULL)";
     let create_cmd = create_table.replacen(":table_name", table_name, 1);
-    conn.query_drop(create_cmd).unwrap();
+    conn.query_drop(create_cmd).expect("Create table failure.");
+}
 
+// ---------------------------------------------------------------------------
+// create_indexes:
+// ---------------------------------------------------------------------------
+fn create_indexes(conn: &mut PooledConn, table_name: &str) {
     // Create indexes on output table.
-    conn.query_drop("CREATE INDEX index_submit ON ".to_owned() + table_name + " (submit)").unwrap();
-    conn.query_drop("CREATE INDEX index_start ON ".to_owned() + table_name + " (start)").unwrap();
-    conn.query_drop("CREATE INDEX index_max_minutes ON ".to_owned() + table_name + " (max_minutes)").unwrap();
-    conn.query_drop("CREATE INDEX index_queue_minutes ON ".to_owned() + table_name + " (queue_minutes)").unwrap();
-    conn.query_drop("CREATE INDEX index_backlog_minutes ON ".to_owned() + table_name + " (backlog_minutes)").unwrap();
-    conn.query_drop("CREATE INDEX index_backlog_num_jobs ON ".to_owned() + table_name + " (backlog_num_jobs)").unwrap();
+    conn.query_drop("CREATE INDEX index_submit ON ".to_owned() + table_name + " (submit)").expect("Create index 1 failure.");
+    conn.query_drop("CREATE INDEX index_start ON ".to_owned() + table_name + " (start)").expect("Create index 2 failure.");
+    conn.query_drop("CREATE INDEX index_max_minutes ON ".to_owned() + table_name + " (max_minutes)").expect("Create index 3 failure.");
+    conn.query_drop("CREATE INDEX index_queue_minutes ON ".to_owned() + table_name + " (queue_minutes)").expect("Create index 4 failure.");
+    conn.query_drop("CREATE INDEX index_backlog_minutes ON ".to_owned() + table_name + " (backlog_minutes)").expect("Create index 5 failure.");
+    conn.query_drop("CREATE INDEX index_backlog_num_jobs ON ".to_owned() + table_name + " (backlog_num_jobs)").expect("Create index 6 failure.");
+}
+
+// ---------------------------------------------------------------------------
+// get_insert_stmt:
+// ---------------------------------------------------------------------------
+fn get_insert_stmt(conn: &mut PooledConn, table_name: &str) -> Statement {
+    // Build the insert statement with placeholders.
+    let insert = "INSERT INTO :table_name \
+            (jobid, submit, start, max_minutes, queue_minutes, backlog_minutes, backlog_num_jobs) \
+            VALUES (:jobid, :submit, :start, :max_minutes, :queue_minutes, :backlog_minutes, :backlog_num_jobs)";
+    let insert_cmd = insert.replacen(":table_name", table_name, 1);
+    let stmt = conn.prep(insert_cmd).expect("Prepare statement failure.");
+
+    // Set automcommit off on this connection so that inserts are committed in bulk.
+    conn.query_drop("SET autocommit=0").expect("Set autocommit off failure.");
+
+    stmt        
+}
+
+// ---------------------------------------------------------------------------
+// insert_output:
+// ---------------------------------------------------------------------------
+fn insert_output(conn: &mut PooledConn, stmt: &Statement, rec: DBtarget) {
+    // Insert a job's record in the output table..
+    match conn.exec_drop(stmt, params! {
+        "jobid" => rec.jobid,
+        "submit" => rec.submit.to_string(),
+        "start" =>  rec.start.to_string(),
+        "max_minutes" => rec.max_minutes,
+        "queue_minutes" => rec.queue_minutes,
+        "backlog_minutes" => rec.backlog_minutes,
+        "backlog_num_jobs" => rec.backlog_num_jobs,
+    }) {
+        Ok(_) => (),
+        Err(e) => {
+            println!("Error writing to database: {}", e);
+            ()
+        }
+    }
 }
 
 // ---------------------------------------------------------------------------
